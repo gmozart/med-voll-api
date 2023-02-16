c772d24d9633035a89a14715139658b3ca8e51e4
gmozart <mozart.gleison@gmail.com>
2023-02-16 13:44:11 -0300

    Desenvolvida a classe MedicoDTO e seus atributos.

---
+++ src/main/java/dto/MedicoDTO.java
@@ -0,0 +1,56 @@
+package dto;
+
+import entity.Endereco;
+import entity.Medico;
+import enums.Especialidade;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Data
+@Builder
+public class MedicoDTO {
+
+    private Long id;
+    private String nome;
+    private Integer telefone;
+    private String email;
+    private Integer crm;
+    private Especialidade especialidade;
+    private Endereco endereco;
+
+    public static MedicoDTO of(Medico medico){
+        return MedicoDTO.builder()
+                .id(medico.getId())
+                .nome(medico.getNome())
+                .telefone(medico.getTelefone())
+                .email(medico.getEmail())
+                .crm(medico.getCrm())
+                .especialidade(medico.getEspecialidade())
+                .endereco(medico.getEndereco())
+                .build();
+    }
+
+    public static Medico of(MedicoDTO medicoDTO){
+        return Medico.builder()
+                .id(medicoDTO.getId())
+                .nome(medicoDTO.getNome())
+                .telefone(medicoDTO.getTelefone())
+                .email(medicoDTO.getEmail())
+                .crm(medicoDTO.getCrm())
+                .especialidade(medicoDTO.getEspecialidade())
+                .endereco(medicoDTO.getEndereco())
+                .build();
+    }
+
+    public static Optional<MedicoDTO> of(Optional<Medico> medico){
+        return medico.stream().map(MedicoDTO::of).findAny();
+    }
+
+    public static List<MedicoDTO> of(List<Medico> medico){
+        return medico.stream().map(MedicoDTO::of).collect(Collectors.toList());
+    }
+}
