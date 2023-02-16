dc54f0115b661e53631ea04133b4ab930fde8ae0
gmozart <mozart.gleison@gmail.com>
2023-02-16 15:13:08 -0300

    Desenvolvida a interface MedicoRepository no pacote repository.

---
+++ src/main/java/repository/MedicoRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}
