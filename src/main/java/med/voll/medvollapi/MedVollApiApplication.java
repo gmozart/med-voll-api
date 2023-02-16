da27d4b1834d62780a796bdde46d35516d700fb9
gmozart <mozart.gleison@gmail.com>
2023-02-16 12:17:01 -0300

    Inicializando API da MED-VOLL

---
+++ src/main/java/med/voll/medvollapi/MedVollApiApplication.java
@@ -0,0 +1,13 @@
+package med.voll.medvollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedVollApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedVollApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/med/voll/medvollapi/MedVollApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.medvollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedVollApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
