66c1c5c7e51bd48c39db7685539fcac8fe6293e4
gmozart <mozart.gleison@gmail.com>
2023-03-24 15:40:47 -0300

    Criada a classe PacienteService e seus metodos CRUD.

---
+++ src/main/java/med/voll/medvollapi/service/PacienteService.java
@@ -0,0 +1,48 @@
+package med.voll.medvollapi.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import med.voll.medvollapi.dto.PacienteDTO;
+import med.voll.medvollapi.repository.PacienteRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PacienteService {
+
+    private final PacienteRepository pacienteRepository;
+
+    public void save(PacienteDTO pacienteDTO){
+        log.info("Criando um novo paciente!");
+        pacienteRepository.save(PacienteDTO.of(pacienteDTO));
+    }
+
+    public Optional<PacienteDTO> findById(Long id){
+        log.info("Retornando um paciente por seu id!");
+        return PacienteDTO.of(pacienteRepository.findById(id));
+    }
+
+    public Optional<List<PacienteDTO>> findAll(){
+        return Optional.of(PacienteDTO.of(pacienteRepository.findAll()));
+    }
+
+    public Page<PacienteDTO> findAllPages(Integer pages, Integer size){
+        return new PageImpl<>(PacienteDTO.of(pacienteRepository.findAll()), PageRequest.of(pages,size), size);
+    }
+
+    public Optional<PacienteDTO> update(Long id, PacienteDTO pacienteDTO){
+        pacienteDTO.setId(id);
+        return Optional.of(PacienteDTO.of(pacienteRepository.save(PacienteDTO.of(pacienteDTO))));
+    }
+
+    public void delete(Long id){
+        pacienteRepository.deleteById(id);
+    }
+}
