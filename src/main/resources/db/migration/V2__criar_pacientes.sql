addea155f64e931d71cebf48db1c41049fceeab2
gmozart <mozart.gleison@gmail.com>
2023-03-30 14:52:54 -0300

    Correção no flyway da taela pacientes

--- src/main/resources/db/migration/V2__criar_pacientes.sql
+++ src/main/resources/db/migration/V2__criar_pacientes.sql
@@ -1,17 +1,17 @@
 CREATE TABLE pacientes(
 
-    id INT PRIMARY KEY NOT NULL,
+    id bigint NOT NULL auto_increment,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL unique,
     cpf VARCHAR(14) NOT NULL unique,
+    cep VARCHAR(10) NOT NULL,
     logradouro VARCHAR(100) NOT NULL,
+    complemento VARCHAR(100) NOT NULL,
     bairro VARCHAR(100) NOT NULL,
-    cep VARCHAR(9) NOT NULL,
-    complemento VARCHAR(100),
-    numero VARCHAR(20),
-    cidade VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
+    numero INT NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
     estado VARCHAR(2) NOT NULL,
-    ativo CHAR(1) NOT NULL
+    ativo CHAR(1) NOT NULL,
+    primary key(id)
 
 );
