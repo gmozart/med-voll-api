c71c58dc781ddcb0a06d47eb5f0e57c9666f2c6a
gmozart <mozart.gleison@gmail.com>
2023-04-01 00:08:59 -0300

    Criada a classe de autenticação.

---
+++ src/main/java/med/voll/medvollapi/service/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package med.voll.medvollapi.service;
+
+import lombok.RequiredArgsConstructor;
+import med.voll.medvollapi.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
