ddf773299edcbeedf5e6460082e11f848eaa56d3
gmozart <mozart.gleison@gmail.com>
2023-04-13 15:06:02 -0300

    Acrescentando novos tratadores de erro.

--- src/main/java/med/voll/medvollapi/exception/TratadorDeExceptions.java
+++ src/main/java/med/voll/medvollapi/exception/TratadorDeExceptions.java
@@ -1,12 +1,16 @@
 package med.voll.medvollapi.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.naming.AuthenticationException;
 import javax.persistence.EntityNotFoundException;
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class TratadorDeExceptions {
@@ -24,6 +28,32 @@ public class TratadorDeExceptions {
     private record DadosErroValidacao(String campo, String mensagem){
         public DadosErroValidacao(FieldError error){
             this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
+
+    private record TratadorDeException(String campo, String mensagem) {
+        public TratadorDeException(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
         }
     }
 
