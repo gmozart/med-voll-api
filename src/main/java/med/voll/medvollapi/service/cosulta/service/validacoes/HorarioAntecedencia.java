912d746be23a3081f86c15ffd5070356de54032a
gmozart <mozart.gleison@gmail.com>
2023-04-25 19:32:48 -0300

    Desenvolvida classe de validação do horario de antecedência da consulta.

---
+++ src/main/java/med/voll/medvollapi/service/cosulta/service/validacoes/HorarioAntecedencia.java
@@ -0,0 +1,25 @@
+package med.voll.medvollapi.service.cosulta.service.validacoes;
+
+import med.voll.medvollapi.exception.ValidacaoException;
+import med.voll.medvollapi.transaction.response.DadosAgendamentoConsulta;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class HorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaMinutos = Duration.between(dataConsulta,agora).toMinutes();
+
+        if(diferencaMinutos < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência  mínima de 30 minutos");
+        }
+
+
+    }
+
+
+}
