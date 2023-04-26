93886820f87042cbfe0bc9757412181f4bea0606
gmozart <mozart.gleison@gmail.com>
2023-04-25 23:06:05 -0300

    Desenvolvido a interface de validação de agendamentos de consulta;

---
+++ src/main/java/med/voll/medvollapi/repository/ValidadorAgendamentoConsultas.java
@@ -0,0 +1,9 @@
+package med.voll.medvollapi.repository;
+
+import med.voll.medvollapi.transaction.response.DadosAgendamentoConsulta;
+
+public interface ValidadorAgendamentoConsultas {
+
+    void validar(DadosAgendamentoConsulta dados);
+
+}
