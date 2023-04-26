49645dd8274abcd64003a7f489b4d2f2254acbd0
gmozart <mozart.gleison@gmail.com>
2023-04-25 22:58:24 -0300

    Desenvolvido a validação do agendamenento do horario da consulta para o paciente.

--- src/main/java/med/voll/medvollapi/service/agendamentoconsulta/service/validacoes/PacienteHorarioConsulta.java
+++ src/main/java/med/voll/medvollapi/service/agendamentoconsulta/service/validacoes/PacienteHorarioConsulta.java
@@ -1,4 +1,23 @@
 package med.voll.medvollapi.service.agendamentoconsulta.service.validacoes;
 
+import lombok.RequiredArgsConstructor;
+import med.voll.medvollapi.exception.ValidacaoException;
+import med.voll.medvollapi.repository.ConsultaRepository;
+import med.voll.medvollapi.transaction.response.DadosAgendamentoConsulta;
+
+@RequiredArgsConstructor
 public class PacienteHorarioConsulta {
+
+    private final ConsultaRepository consulta;
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var medicoPossuiConsulta = consulta.existsByPacienteIdAndDataBetween(dados.idPaciente(), primeiroHorario, ultimoHorario);
+
+        if (medicoPossuiConsulta){
+            throw  new ValidacaoException("Paciente já possui consulta agendada neste horario!");
+        }
+    }
 }
