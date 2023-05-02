b30b9cd8200accadd14cc8a2d3a8d713a6e60625
gmozart <mozart.gleison@gmail.com>
2023-05-02 15:49:56 -0300

    Inserindo o atributo telefone

--- src/main/resources/db/migration/V2__criar_pacientes.sql
+++ src/main/resources/db/migration/V2__criar_pacientes.sql
@@ -5,6 +5,7 @@ CREATE TABLE pacientes(
     email VARCHAR(100) NOT NULL unique,
     cpf VARCHAR(14) NOT NULL unique,
     cep VARCHAR(10) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
     logradouro VARCHAR(100) NOT NULL,
     complemento VARCHAR(100) NOT NULL,
     bairro VARCHAR(100) NOT NULL,
